916b1cd0a14f998fa2b908623922e36a02968c84
zhangchao <nezhaat78@gmail.com>
2023-10-25 20:33:06 +0800

    add: train script

---
+++ tools/videoevent_slowfast_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "start training"
+CUDA_VISIBLE_DEVICES=$1 python train.py ./configs/videoevent_slowfast.yaml --output pretrained
+echo "start testing..."
+CUDA_VISIBLE_DEVICES=$1 python eval.py ./configs/videoevent_slowfast.yaml ckpt/videoevent_slowfast_pretrained/epoch_010.pth.tar
+
