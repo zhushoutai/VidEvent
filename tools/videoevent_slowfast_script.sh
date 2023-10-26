2372bdd2221c99cc2f5969d10c062a529f678c9c
zhangchao <nezhaat78@gmail.com>
2023-10-26 10:50:09 +0800

    update: change anno

--- tools/videoevent_slowfast_script.sh
+++ tools/videoevent_slowfast_script.sh
@@ -3,5 +3,5 @@
 echo "start training"
 CUDA_VISIBLE_DEVICES=$1 python train.py ./configs/videoevent_slowfast.yaml --output pretrained
 echo "start testing..."
-CUDA_VISIBLE_DEVICES=$1 python eval.py ./configs/videoevent_slowfast.yaml ckpt/videoevent_slowfast_pretrained/epoch_010.pth.tar
+CUDA_VISIBLE_DEVICES=$1 python eval.py ./configs/videoevent_slowfast.yaml ckpt/videoevent_slowfast_pretrained/epoch_014.pth.tar
 
